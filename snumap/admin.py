10b6558577041caf3d6ba85125671f8a5c7620a1
Dorvin <ladig1017@gmail.com>
2019-04-24 20:40:56 +0900

    modify model and add to admin page

--- snumap/admin.py
+++ snumap/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Map, Spot, Edge, Shuttle, Route, Building, Restaurant, Seminar, Lecture, Post
 # Register your models here.
+
+admin.site.register([Map, Spot, Edge, Shuttle, Route, Building, Restaurant, Seminar, Lecture, Post])
--- snumap/models.py
+++ snumap/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Map(models.Model):
-    link = models.URLField()
+    link = models.URLField(max_length=200)
 
 class Spot(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
@@ -14,51 +14,51 @@ class Edge(models.Model):
     spots = models.ManyToManyField(Spot, related_name='edges')
 
 class Shuttle(models.Model):
-    kr_name = models.CharField()
-    en_name = models.CharField()
+    kr_name = models.CharField(max_length=200)
+    en_name = models.CharField(max_length=200)
     operating_hours = models.TextField()
     info = models.TextField()
     edges = models.ManyToManyField(Edge, related_name='shuttles')
 
 class Route(models.Model):
-    start = models.ForeignKey(Spot , on_delete=models.CASCADE)
-    end = models.ForeignKey(Spot , on_delete=models.CASCADE)
+    start = models.ForeignKey(Spot , related_name='start_for', on_delete=models.CASCADE)
+    end = models.ForeignKey(Spot , related_name='end_for', on_delete=models.CASCADE)
     length = models.FloatField()
     edges = models.ManyToManyField(Edge)
 
 class Building(models.Model):
-    code = models.CharField()
-    kr_name = models.CharField()
-    en_name = models.CharField()
+    code = models.CharField(max_length=200)
+    kr_name = models.CharField(max_length=200)
+    en_name = models.CharField(max_length=200)
     spot = models.ForeignKey(Spot, related_name='building', on_delete=models.CASCADE)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     info = models.TextField()
 
 class Restaurant(models.Model):
-    code = models.CharField()
-    kr_name = models.CharField()
-    en_name = models.CharField()
+    code = models.CharField(max_length=200)
+    kr_name = models.CharField(max_length=200)
+    en_name = models.CharField(max_length=200)
     building = models.ForeignKey(Building, related_name='restaurants', on_delete=models.CASCADE)
     operating_hours = models.TextField()
 
 class Seminar(models.Model):
-    title = models.CharField()
-    description = models.CharField()
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
     building = models.ForeignKey(Building, related_name='seminars', on_delete=models.CASCADE)
-    time = models.CharField()
+    time = models.CharField(max_length=200)
 
 class Lecture(models.Model):
-    code = models.CharField()
-    title = models.CharField()
-    professor = models.CharField()
-    link = models.URLField()
+    code = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    professor = models.CharField(max_length=200)
+    link = models.URLField(max_length=200)
     building = models.ForeignKey(Building, related_name='lectures', on_delete=models.CASCADE)
-    time = models.CharField()
+    time = models.CharField(max_length=200)
 
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    username = models.CharField()
-    password = models.CharField()
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
     building = models.ForeignKey(Building, related_name='posts', on_delete=models.CASCADE)
