c36c9c989955c19588bd19d86d9b542dcb290aa0
pistolstar1797 <ehdgkr1797@naver.com>
2019-06-04 17:14:41 +0900

    경제학부 세미나 추가

--- crawler.py
+++ crawler.py
@@ -61,3 +61,65 @@ def cseSeminar():
         ).save()
 
 cseSeminar()
+
+def econSeminar():
+    import requests
+    from bs4 import BeautifulSoup
+    import re
+    from snumap.models import Map, Spot, Edge, Shuttle, Route, Building, Restaurant, Seminar, Lecture, Post
+    req = requests.get('http://econ.snu.ac.kr/research/seminars/list-view')
+    if not req.ok:
+        print("Error: request to cseSeminar has failed")
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    Seminars = soup.select(
+        '#zcmsprogram'
+    )[0]
+    seminarList = Seminars.select('div.seminarlist.clearfix')
+    for seminar in seminarList:
+        link = "http://econ.snu.ac.kr"+seminar.select('a')[0]['href']
+        try:
+            existing_seminar = Seminar.objects.get(link=link)
+        except Seminar.DoesNotExist:
+            pass
+        else:
+            continue
+        detailReq = requests.get(link)
+        detailHtml = detailReq.text
+        detailSoup = BeautifulSoup(detailHtml, 'html.parser')
+        detailContents = detailSoup.select('div.seminarview')[0]
+        title = detailContents.find('h3').text
+        talkerData = detailContents.select('div')[2]
+        talker = talkerData.find(text=True)
+        talkerFrom = talkerData.find('div')
+        for br in talkerFrom.find_all("br"):
+            br.replace_with("-")
+        if talkerFrom.text is not None:
+            talker = talker+"-"+talkerFrom.text
+        time = detailContents.select('div')[1].text
+        where = detailContents.select('div')[3].text
+        description = detailContents.select('div')[4].text
+        codePattern = re.compile('\d+')
+        codeMatch = codePattern.search(where)
+        code = ""
+        if codeMatch:
+            code = codeMatch.group()
+        else:
+            print("no code is found in where information")
+        try:
+            building = Building.objects.get(code__startswith=code)
+        except Building.DoesNotExist:
+            print("error: no such a Building code:"+code+"/")
+        except Building.MultipleObjectsReturned:
+            print("error: ther is duplicated Building that has the code:"+code)
+        Seminar(
+            title=title,
+            talker=talker,
+            description=description,
+            building=building,
+            where=where,
+            time=time,
+            link=link
+        ).save()
+
+econSeminar()
